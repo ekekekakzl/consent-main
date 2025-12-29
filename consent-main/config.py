52c4cc26a3bca1286083153d8eea4774bee8e674
ekekekakzl <ekekekakzl@snu.ac.kr>
2025-12-29 23:16:51 +0900

    Update config.py

--- consent-main/config.py
+++ consent-main/config.py
@@ -43,7 +43,7 @@ HARDCODED_BASE_EXPLANATIONS = {
     - **콘솔**
     <br>: 의사가 3D 화면을 보며 로봇 팔을 <mark><span style="color: #007bff;">**직접**</span></mark> 조종하여 진행합니다.
     - **환자카트**
-    <br>: 환자 옆에 위치하며, 카메라와 수술 기구가 달린 <mark><span style="color: #007bff;">**4개의 로봇 팔**</span></mark>이 있습니다.
+    <br>: 카메라와 수술 기구가 달린 <mark><span style="color: #007bff;">**4개의 로봇 팔**</span></mark>이 있습니다.
     - **비전카트**
     <br>: 수술 중 의료진 모두가 <mark><span style="color: #007bff;">**화면을 공유**</span></mark>할 수 있도록 도와줍니다.
         """,
@@ -56,7 +56,7 @@ HARDCODED_BASE_EXPLANATIONS = {
     - **콘솔**
     <br>: 의사가 3D 화면을 보며 로봇 팔을 <mark><span style="color: #007bff;">**직접**</span></mark> 조종하여 진행합니다.
     - **환자카트**
-    <br>: 환자 옆에 위치하며, 카메라와 수술 기구가 달린 <mark><span style="color: #007bff;">**4개의 로봇 팔**</span></mark>이 있습니다.
+    <br>: 카메라와 수술 기구가 달린 <mark><span style="color: #007bff;">**4개의 로봇 팔**</span></mark>이 있습니다.
     - **비전카트**
     <br>: 수술 중 의료진 모두가 <mark><span style="color: #007bff;">**화면을 공유**</span></mark>할 수 있도록 도와줍니다.
 
@@ -70,8 +70,8 @@ HARDCODED_BASE_EXPLANATIONS = {
         |<span style="color: #007bff;">**장점**</span>|<span style="color: #dc143c;">**단점**</span>|
         |---|---|
         |상처가 작고 아픔이 적습니다.<br> <span style="color: #007bff;">**회복이 빠릅니다.**</span>|건강보험이 적용되지 않아 <br><span style="color: #dc143c;">**수술비가 비쌉니다.**</span>
-        화면을 확대해서 볼 수 있고,<br>로봇 팔이 자유롭게 움직여 <br><span style="color: #007bff;">**정교한 수술**</span>이 가능합니다.|로봇 세팅 시간이 추가되어<br><span style="color: #dc143c;">**수술 시간이 조금 더**</span> 걸립니다.
-        손 떨림 없이 미세한 작업이 가능해<br> 주변의 혈관이나 신경 <span style="color: #007bff;">**손상을 최소화**</span>할 수 있습니다.|수술 중 예상치 못한 문제가 생기면 <br><span style="color: #dc143c;">**배를 열거나 복강경 수술로**</span> 바뀔 수 있습니다.|
+        화면을 확대해서 볼 수 있고,<br>로봇 팔이 자유롭게 움직여 <br><span style="color: #007bff;">**정교한 수술**</span>이 가능합니다.|로봇세팅시간이 추가되어<br><span style="color: #dc143c;">**수술 시간이 조금 더**</span> 걸립니다.
+        손떨림 없이 미세한 작업이 가능해<br> 주변 혈관이나 신경 <span style="color: #007bff;">**손상을 최소화**</span>할 수 있습니다.|수술 중 문제가 생기면 <br><span style="color: #dc143c;">**배를 열거나 복강경 수술로**</span> 바뀔 수 있습니다.|
 
         """,
 
@@ -83,7 +83,7 @@ HARDCODED_BASE_EXPLANATIONS = {
         |---|---|
         |상처가 작고 아픔이 적습니다.<br> <span style="color: #007bff;">**회복이 빠릅니다.**</span>|건강보험이 적용되지 않아 <br><span style="color: #dc143c;">**수술비가 비쌉니다.**</span>
         화면을 확대해서 볼 수 있고, <br>로봇 팔이 자유롭게 움직여 <span style="color: #007bff;">**정교한 수술**</span>이 가능합니다.|로봇 세팅 시간이 추가되어<br><span style="color: #dc143c;">**수술 시간이 조금 더**</span> 걸립니다.
-        발기 관련 신경을 살리거나 <span style="color: #007bff;">**방광과 요도를 다시 연결**</span>하는 <br>중요한 과정을 더 <span style="color: #007bff;">**정확하게**</span> 할 수 있습니다.|수술 중 예상치 못한 문제가 생기면<br><span style="color: #dc143c;">**배를 여는 수술로**</span> 바뀔 수 있습니다.|
+        발기 관련 신경을 살리거나 <span style="color: #007bff;">**방광과 요도를 다시 연결**</span>하는 <br>중요한 과정을 더 <span style="color: #007bff;">**정확하게**</span> 할 수 있습니다.|수술 중 문제가 생기면<br><span style="color: #dc143c;">**배를 여는 수술로**</span> 바뀔 수 있습니다.|
 
 
         """
@@ -94,7 +94,7 @@ HARDCODED_BASE_EXPLANATIONS = {
         """
         ❶ 배꼽 주변에 2cm 정도의 작은 구멍을 뚫고 관을 넣습니다.
         <br>❷ <mark>**이산화탄소 가스**</mark>를 넣어 배를 부풀려 수술할 공간을 만듭니다.
-        <br>❸ 카메라와 수술 도구가 들어갈 0.5-1cm 크기의 구멍을 아랫배에 <mark><span style="color: #dc143c;">**2~3개 더**</span></mark> 뚫습니다.<br>(구멍 위치와 개수는 달라질 수 있습니다.)
+        <br>❸ 카메라와 수술 도구가 들어갈 0.5-1cm 크기의 구멍을 아랫배에 <mark><span style="color: #dc143c;">**2~3개 더**</span></mark> 뚫습니다.(구멍 위치와 개수는 달라질 수 있습니다.)
         <br>❹ 떼어낸 자궁 등은 질을 통해 꺼내거나, 잘게 잘라서 꺼냅니다.
         <br>❺ 구멍을 뚫었던 부위를 꿰매고 수술을 마칩니다.
         
@@ -124,13 +124,13 @@ HARDCODED_BASE_EXPLANATIONS = {
     "알아두어야할 점": {
         "로봇보조 자궁절제술": """
 
-        - 수술 전 검사에서 종양이 크거나 장기가 심하게 달라붙어 있을 것으로 예상되면, 
+        - 종양이 크거나 장기가 심하게 달라붙어 있을 것으로 예상되면, 
         <br><mark><span style="color: #dc143c;">**처음부터 배를 여는 수술**</span></mark>을 할 수도 있습니다.
 
-        - 로봇 수술 중에 피가 많이 나거나 주변 장기가 다칠 위험이 크다고 판단되면, 
+        - 수술 중에 피가 많이 나거나 주변 장기가 다칠 위험이 크다고 판단되면, 
         <br><mark><span style="color: #dc143c;">**안전을 위해 배를 여는 수술**</span></mark>로 바꿀 수 있습니다.
 
-        - 암이 다른 장기(소화기관, 비뇨기관 등)로 퍼졌거나 수술 중 손상이 생기면, 
+        - 다른 장기(소화기관, 비뇨기관 등)에 손상이 생기면, 
         <br><mark><span style="color: #dc143c;">**다른 과 의사와 함께 수술**</span></mark>하며 수술 범위가 커질 수 있습니다.
 
         - 수술 중 **림프절이나 난소를 함께 제거**할 수도 있습니다.
@@ -163,6 +163,7 @@ SECTIONS_SIDEBAR_MAP = {
     "considerations": {"title": "수술은 어떻게 진행되나요?", "idx": 3},
     "side_effects": {"title": "알아두어야할 점", "idx": 4},
 }
+
 
 
 
